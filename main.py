1c48defb5cccdd7bb299acc27ecf7c97a87d9a11
fall3n <aka.fall3n@gmail.com>
2023-05-05 11:51:35 +0300

    first version

---
+++ main.py
@@ -0,0 +1,70 @@
+import csv
+import os
+import zipfile
+from argparse import ArgumentParser
+
+from sqlalchemy import inspect, select
+
+from db import Model, Session, engine
+from models import Address, Order, User, UserRole
+
+
+def object_as_dict(obj):
+    return {c.key: getattr(obj, c.key)
+            for c in inspect(obj).mapper.column_attrs}
+
+
+def do_import(path: str):
+    Model.metadata.drop_all(engine)  # warning: this deletes all data!
+    Model.metadata.create_all(engine)
+
+    with Session() as session:
+        with session.begin():
+            with open(path) as f:
+                reader = csv.DictReader(f, delimiter=';')
+                for row in reader:
+                    role_type = row.pop('role_type')
+                    role = UserRole(role_type=role_type)
+                    user_name = row.pop('first_name')
+                    user_l_name = row.pop('last_name')
+                    addr = Address(**row)
+                    user = User(first_name=user_name, last_name=user_l_name)
+                    session.add(user)
+                    user.users_roles.append(role)
+                    user.addresses.append(addr)
+    os.remove(path)
+    print('Import successful')
+
+
+def do_export(path: str):
+    with Session() as session:
+        with session.begin():
+            with open(path, 'w', newline='') as f:
+                query = select(User)
+                fieldnames = list([column.name for column in User.__table__.columns])
+                writer = csv.DictWriter(f, fieldnames=fieldnames, delimiter=';')
+                writer.writeheader()
+                for row in session.execute(query):
+                    dct = object_as_dict(row[0])
+                    writer.writerow(dct)
+    print('Export successful')
+    with zipfile.ZipFile("test.zip", mode="w") as archive:
+        archive.write(path)
+    print('Zip file created')
+
+
+def main():
+    parser = ArgumentParser(description='Upload and download content of User database')
+    parser.add_argument('-i', '--do_import', action='store_true', help='Import content of CSV file to Users DB')
+    parser.add_argument('-e', '--do_export', action='store_true', help='Export content of Users DB to CSV file')
+    parser.add_argument('path')
+    args = parser.parse_args()
+    if args.do_import:
+        do_import(args.path)
+    elif args.do_export:
+        do_export(args.path)
+
+
+if __name__ == '__main__':
+    main()
+
---
+++ models.py
@@ -0,0 +1,68 @@
+from sqlalchemy import ForeignKey, Integer, String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from db import Model
+
+
+class Address(Model):
+    __tablename__ = 'addresses'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    country: Mapped[str] = mapped_column(String(64))
+    city: Mapped[str] = mapped_column(String(64))
+    street: Mapped[str] = mapped_column(String(64))
+    building: Mapped[str] = mapped_column(String(64))
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey('users.id'), index=True)
+    user: Mapped['User'] = relationship(
+        back_populates='addresses')
+
+    def __repr__(self):
+        return f'Address({self.id}, "{self.country}", "{self.city}", "{self.street}", "{self.building}",)'
+
+
+class UserRole(Model):
+    __tablename__ = 'users_roles'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    role_type: Mapped[int] = mapped_column(Integer)  # 1-Free, 2-Paid
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey('users.id'), index=True)
+    user: Mapped['User'] = relationship(
+        back_populates='users_roles')
+
+    def __repr__(self):
+        return f'UserRole({self.id}, "{self.role_type}",)'
+
+
+class Order(Model):
+    __tablename__ = 'orders'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    status: Mapped[int] = mapped_column(Integer)  # 1-Created,2-Payd,3-Canceled
+    total: Mapped[int] = mapped_column(Integer)
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey('users.id'), index=True)
+    user: Mapped['User'] = relationship(
+        back_populates='orders')
+
+    def __repr__(self):
+        return f'Order({self.id}, "{self.total}",)'
+
+
+class User(Model):
+    __tablename__ = 'users'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    first_name: Mapped[str] = mapped_column(String(64))
+    last_name: Mapped[str] = mapped_column(String(64))
+
+    addresses: Mapped[list['Address']] = relationship(
+        cascade='all, delete-orphan', back_populates='user')
+    users_roles: Mapped[list['UserRole']] = relationship(
+        cascade='all, delete-orphan', back_populates='user')
+    orders: Mapped[list['Order']] = relationship(
+        cascade='all, delete-orphan', back_populates='user')
+
+    def __repr__(self):
+        return f'User({self.id}, "{self.first_name}", "{self.last_name}")'
